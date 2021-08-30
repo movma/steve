e957c26997722e9f2a4fc13ae3613a1aed1c30a8
mania-76 <89701401+mania-76@users.noreply.github.com>
2021-08-30 08:12:50 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '❌Join our group❌', url="https://t.me/aa07100"
                                 )
                         ]
                     ]
